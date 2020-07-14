c6248fb6515d9c265ba34bc61e500a095b248c0e
i-m-avi <avinashbb695@gmail.com>
2020-07-15 00:24:03 +0530

    Palindrome Number LC Problem

---
+++ LeetCodeProblems/palindromeNumber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x!=0))
+        {
+            return false;
+        }
+        int halfrev = 0;
+        while(x > halfrev)
+        {
+            halfrev = halfrev*10 + (x%10);
+            x/=10;
+        }
+        return x == halfrev || x == halfrev/10;
+        
+    }
+}
